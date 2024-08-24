ff220ae0fdd5de2453ea2c370298bef44a46a2c6
evmcheb <evmcheb@users.noreply.github.com>
2024-08-24 11:27:51 +0800

    feat: remappings

--- test/01-vault.sol
+++ test/01-vault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test {
+contract Vault is Test {
     Counter public counter;
 
     function setUp() public {
